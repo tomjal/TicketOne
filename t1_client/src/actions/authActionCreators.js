c28c13f162152341831a2047d72674496228c9f4
tomjal <jaloszynski@live.com>
2017-04-16 03:32:03 +0200

    roles import in actions

--- t1_client/src/actions/authActionCreators.js
+++ t1_client/src/actions/authActionCreators.js
@@ -1,16 +1,19 @@
 import { checkIfFetchStatusOk, dispatchFetchError } from "./actionFetchHelpers";
 import { apiSchema } from "./../api/apiSchema";
 import { authActions } from "./actionTypes";
+import { ROLES } from './../consts/roles';
+
+const MASTER_EMPLOYEE_NAME = 'Employee_TicketOne';
 
 export function setApplicationContextAsMockClient(ident) {
-    const mockAuth = { role: "client", id: ident };
+    const mockAuth = { role: ROLES.CLIENT, id: ident };
     return (dispatch) => {
         dispatch({ type: authActions.GET_AUTH_CONTEXT.SUCCESS, data: mockAuth })
     };
 }
 
 export function setApplicationContextAsMockEmployee() {
-    const mockAuth = { role: "employee", id: "Employee_TicketOne" };
+    const mockAuth = { role: ROLES.EMPLOYEE, id: MASTER_EMPLOYEE_NAME };
     return (dispatch) => {
         dispatch({ type: authActions.GET_AUTH_CONTEXT.SUCCESS, data: mockAuth })
     };
