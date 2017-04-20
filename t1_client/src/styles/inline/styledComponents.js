8844214d599665088d83dedab539346e5283d47f
tomjal <jaloszynski@live.com>
2017-04-20 03:44:42 +0200

    ToDo, styled component

---
+++ t1_client/src/styles/inline/styledComponents.js
@@ -0,0 +1,36 @@
+/*
+import { ThemeProvider } from 'styled-components';
+
+const blackTheme = {
+  main: 'black',
+};
+
+export const StyleTheme = (props) => {
+  return (
+    <ThemeProvider theme={blackTheme}>
+      {props.children}
+    </ThemeProvider>
+  );
+}
+
+import styled from 'styled-components';
+
+export const ThreeColumnsStyle = styled.div`
+  position: absolute;
+  top: 0px;
+  bottom: 0px;
+  left: 0px;
+  right: 0px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  & > div:nth-of-type(1) {
+    flex: 1 1 200px;
+  }
+  & > div:nth-of-type(2) {
+    flex: 0 0 500px;
+  }
+  & > div:nth-of-type(3) {
+    flex: 1 1 200px;
+  }
+`*/
