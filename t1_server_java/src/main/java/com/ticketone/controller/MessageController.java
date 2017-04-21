72c64689ef89907228bd2ca9ac6ed547e922947f
tomjal <jaloszynski@live.com>
2017-04-21 13:02:20 +0200

    missing deps

--- t1_server_java/src/main/java/com/ticketone/controller/MessageController.java
+++ t1_server_java/src/main/java/com/ticketone/controller/MessageController.java
@@ -1,5 +1,10 @@
 package com.ticketone.controller;
 
+import com.ticketone.facade.TransactionFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/api/v1/message")
 public class MessageController {
