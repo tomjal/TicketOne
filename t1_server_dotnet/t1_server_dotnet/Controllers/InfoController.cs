7f0ffe13ac1e3f527ec75056f771beaa37e0d5cf
tomjal <jaloszynski@live.com>
2017-11-15 01:05:22 +0100

    minimal ASP .NET core API

---
+++ t1_server_dotnet/t1_server_dotnet/Controllers/InfoController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace t1_server_dotnet.Controllers
+{
+    [Route("api/[controller]")]
+    public class InfoController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "version_1", "status_alive" };
+        }
+    }
+}
